46872cdbae9ca2f9b4cb52011bcedf741af362b8
31122001ntp <80078251+31122001ntp@users.noreply.github.com>
2021-04-05 16:30:32 +0700

    Create student.cs

---
+++ student.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AppOOP
+{
+
+    class Student
+    {
+        private string Name;
+
+        public string StudentName
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+
+        public Student()
+        {}
+        {
+            Name = stName;
+        }
+        public virtual void Talk()
+        {
+            MessageBox.Show("NguyenThanhPhuong");
+        }
+
+    }
+}
